c7cca4f47b0657a4c0f9bb4158ec4c08f1518162
Liam Wise <wiseliam7@gmail.com>
2022-07-24 11:58:52 +1000

    docs

--- server/matching/matchAlgo.js
+++ server/matching/matchAlgo.js
@@ -1,4 +1,4 @@
-const geo = require('geolib');
+const geo = require("geolib");
 
 // Algorithm to find recommended matches - currently hardcoded based off:
 // 	location (within 15km)
@@ -6,22 +6,25 @@ const geo = require('geolib');
 // 	Age gap (within 5 years)
 // 	Skill rating (ommitted as not included in seed data)
 const isMatch = (user1, user2) => {
-	const withinRadius = geo.isPointWithinRadius(user1.location, user2.location, 15000);
-	let sameGenre = false;
-	for (let genre of user1.genres) {
-		if (user2.genres.includes(genre)) {
-			sameGenre = true
-		}
-	}
-	const user1DOB = new Date(user1.dateOfBirth);
-	const user2DOB = new Date(user2.dateOfBirth);
-	const ageDiff = user1DOB.getFullYear() - user2DOB.getFullYear()
-	const skillDiff = Math.abs(user1.skillLevel - user2.skillLevel);
-	if (withinRadius && sameGenre && ageDiff < 5) {
-		return true
-	} 
-	return false
-}
-
+  const withinRadius = geo.isPointWithinRadius(
+    user1.location,
+    user2.location,
+    15000
+  );
+  let sameGenre = false;
+  for (let genre of user1.genres) {
+    if (user2.genres.includes(genre)) {
+      sameGenre = true;
+    }
+  }
+  const user1DOB = new Date(user1.dateOfBirth);
+  const user2DOB = new Date(user2.dateOfBirth);
+  const ageDiff = user1DOB.getFullYear() - user2DOB.getFullYear();
+  const skillDiff = Math.abs(user1.skillLevel - user2.skillLevel);
+  if (withinRadius && sameGenre && ageDiff < 5) {
+    return true;
+  }
+  return false;
+};
 
 module.exports = isMatch;
